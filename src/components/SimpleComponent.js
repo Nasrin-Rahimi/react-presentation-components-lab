a2c4f96f87ff5acd44d663f96c77bb4c103ea7f2
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-03-31 07:49:00 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,29 @@
-// Code SimpleComponent Here
+import React from 'react'
+
+class SimpleComponent extends React.Component{
+
+    constructor(){
+        super()
+        this.state = {
+            mood: 'happy'
+        }
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            return{
+                mood: previousState.mood === 'happy' ? 'sad' : 'happy'
+            }
+        })
+    }
+
+    render(){
+        return(
+            <div onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        )
+    }
+}
+
+export default SimpleComponent
